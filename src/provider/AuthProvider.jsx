c8d408ee70cac0147910c5960907771737bc0687
jahidul39306 <jahidulislamananto@gmail.com>
2025-01-15 03:06:24 +0600

    added auth provider

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,89 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, updateProfile, signInWithPopup, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import useAxiosPublic from "../hooks/useAxiosPublic";
+
+const GlobalContext = createContext();
+
+const AuthProvider = ({ children }) => {
+
+    const auth = getAuth(app);
+    const [user, setUser] = useState(auth.currentUser);
+    const [loading, setLoading] = useState(true);
+    const axiosPublic = useAxiosPublic();
+
+    // variables for google signin login
+    const googleProvider = new GoogleAuthProvider();
+
+    // create user with email
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // update profile
+    const updateUserProfile = (updatedProfile) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, updatedProfile);
+    }
+
+    // google sigin
+    const createUserWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    // login user with email
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // google login
+    const loginWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    // logout user
+    const userLogOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const globalInfo = {
+        createUser,
+        user,
+        setUser,
+        loading,
+        setLoading,
+        updateUserProfile,
+        createUserWithGoogle,
+        loginUser,
+        loginWithGoogle,
+        userLogOut,
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+            setUser(currentUser);
+            if (currentUser?.email) {
+                // const user = { email: currentUser.email };
+                setLoading(false);
+            }
+            else {
+                setLoading(false);
+            }
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, [auth, axiosPublic]);
+
+    return (
+        <GlobalContext.Provider value={globalInfo}>{children}</GlobalContext.Provider>
+    );
+};
+
+export default AuthProvider;

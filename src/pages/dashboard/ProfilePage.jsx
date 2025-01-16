2b3600be9c6c3237c446d9ae0fbe186a651d5808
jahidul39306 <jahidulislamananto@gmail.com>
2025-01-16 07:08:57 +0600

    user profile page complete

--- src/pages/dashboard/ProfilePage.jsx
+++ src/pages/dashboard/ProfilePage.jsx
@@ -1,8 +1,50 @@
+import { useContext } from 'react';
+import avatar from '../../assets/avatar.jpg';
+import { GlobalContext } from '../../provider/AuthProvider';
+import Loading from '../../components/Loading';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const ProfilePage = () => {
+    const { user } = useContext(GlobalContext);
+
+    const axiosSecure = useAxiosSecure();
+
+
+    const fetchUser = async () => {
+        const { data } = await axiosSecure.get(`/user/${user.email}`);
+        return data;
+    };
+
+    const { data: userData, isFetching } = useQuery({
+        queryKey: ["userData", user.email],
+        queryFn: () => fetchUser()
+    });
+
+    if (isFetching) {
+        return <Loading></Loading>
+    }
+
     return (
-        <div>
-            This is profile
+        <div className='flex flex-col md:flex-row items-center p-5 gap-10'>
+            <div>
+                <img
+                    src={user.photoURL || avatar}
+                    alt=""
+                    onError={(e) => {
+                        e.target.src = avatar;
+                    }}
+                    className='w-[150px]'
+                />
+            </div>
+            <div className='text-lg font-semibold text-blue-500'>
+                <p>Name: {user.displayName}</p>
+                <p>Email: {user.email}</p>
+                {
+                    (userData?.role && <p>Role: {userData.role}</p>)
+                }
+
+            </div>
         </div>
     );
 };
